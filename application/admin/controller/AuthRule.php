db07ec29b92bd8593706f0af20840da115c4a950
王胖子 <1776643730@qq.com>
2021-01-09 09:39:15 +0800

    组的添加‘

--- application/admin/controller/AuthRule.php
+++ application/admin/controller/AuthRule.php
@@ -14,51 +14,46 @@ class AuthRule extends Common
         return View::fetch('admin-rule');
     }
     //添加顶级
-    //需要插入字段  name title ico
+    //需要插入字段  name title ico status(可选)
     public function ruleAdd()
     {
-        $id = $this->request->param();
-        if ($id) {
-            if ((request()->isPost())) {
-                return $id;
-//                $data = $this->request->param();
-//                if ($data['pid']) {
-//                    $data['fid'] = 1;
-//                } else {
-//                    $data['fid'] = 2;
-//                }
-//                return $data;
-//            $modelAuthRule = new ModelAuthRule();
-//            $resMdelo = $modelAuthRule->ruleAdd($data);
-////            return  $resMdelo;
-//            if ($resMdelo == 1) {
-//                $this->success('添加成功');
-//            } else {
-//                $this->error($resMdelo);
-//            }
-//            }
-            }
-        } else {
-            if ((request()->isPost())) {
-                return $id;
-//                $data = $this->request->param();
-//                return  $data;
-//            $modelAuthRule = new ModelAuthRule();
-//            $resMdelo = $modelAuthRule->ruleAdd($data);
-////            return  $resMdelo;
-//            if ($resMdelo == 1) {
-//                $this->success('添加成功');
-//            } else {
-//                $this->error($resMdelo);
-//            }
-//            }
+        if ((request()->isPost())) {
+            $data = $this->request->param();
+            $modelAuthRule = new ModelAuthRule();
+            $resMdelo = $modelAuthRule->ruleAdd($data);
+            if ($resMdelo == 1) {
+                $this->success('添加成功');
+            } else {
+                $this->error($resMdelo);
             }
         }
-
-
-        View::assign('DingJi', $id);
-
         return View::fetch('rule-add');
+    }
+
+    //二级权限添加
+    //需要插入字段  name title  status(可选) fid  pid
+    public function levelAdd()
+    {
+        $id = input('id');
+        $listFid = Db::table('think_auth_rule')->find($id);
+        if ((request()->isPost())) {
+            $data = $this->request->param();
+            if($data['fid']){
+                $data['fid'] = 2;
+            }else{
+                $data['fid'] = 1;
+            }
+//            return $data;
+            $modelAuthRule = new ModelAuthRule();
+            $resMdelo = $modelAuthRule->ruleAdd($data);
+            if ($resMdelo == 1) {
+                $this->success('添加成功');
+            } else {
+                $this->error($resMdelo);
+            }
+        }
+        View::assign('level', $listFid);
+        return View::fetch('level-add');
     }
 
     //删除
